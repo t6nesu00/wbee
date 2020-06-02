fbff9631ea0e0933b5e3f361fb8863417ebc2360
Suman Nepali <hlw.zigri@gmail.com>
2020-06-02 21:13:22 +0300

    fetched result in teachers page

--- wbee/users/student/dashboard.php
+++ wbee/users/student/dashboard.php
@@ -1,117 +1,50 @@
 <?php
-	include '../../resources/config.php';
+	require '../../resources/config.php';
 	if(empty($_SESSION['email']))
 		header('Location: ../../login.php');
 ?>
+
 
 <html>
 <head><title>Organization Dashboard</title>
 <!-- css links -->
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="sstyle.css">
+	<link rel="stylesheet" href="tstyle.css">
 </head>
+	
+<body style="background-color: orange;">
 	<div>
-		<?php include "sheader.php"; ?>
+		<?php include 'theader.php'; ?>
 	</div>
-
-	<div class="container">
-		<div class="card text-center">
-			<div class="card-header">
-			<div class="col-lg-2 col-lg-push-10" style="float: right;">
-				<div id="active_question" style="float:left">0</div>
-				<div style="float:left">/</div>  
-				<div id="total_question" style="float:left">0</div>
-			</div>
-			</div>
-			<!-- load questions -->
-		<div class="card-body">
-		<div class="row" style="margin-top: 10px">
-			<div class="row">
-				<div class="col-lg-10 col-lg-push-1" style="min-height: 200px; background-color: white" id="load_questions"></div>
-			</div>
-		</div>
-		</div>
-		<div class="card-footer">
-		<div class="col-lg-6">
-				<div class="col-lg-12">
-					<input type="button" class="btn btn-warning" value="Previous" onclick="load_previous();"> &nbsp; 
-					<input type="button" class="btn btn-success" value="Next" onclick="load_next();">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3">
+				<div class="wrapper">
+    				<div class="sidebar">
+        				<?php include 'tsidebar.php'; ?>
+    				</div>
 				</div>
 			</div>
-		</div>
+			<div class="col-9">
+            <div class="card">
+                <div class="card-header">
+                    Exam result
+                </div>
+                <div class="card-body">
+                
+                </div>
+            </div>
+
+					
 		</div>
 	</div>
-
-	<script>
-		function load_total_question(exam_category)
-			{
-				var xmlhttp=new XMLHttpRequest();
-				xmlhttp.onreadystatechange=function() {
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						document.getElementById("total_question").innerHTML = xmlhttp.responseText;
-					}
-				};
-				xmlhttp.open("GET","../../ajax/load_total_question.php",true);
-				xmlhttp.send(null);
-			}
-
-			var questionNo = "1";
-			load_questions(questionNo);
-
-			function load_questions(questionNo)
-			 {
-				document.getElementById("active_question").innerHTML = questionNo;
-				var xmlhttp=new XMLHttpRequest();
-				xmlhttp.onreadystatechange=function() {
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						if(xmlhttp.responseText=="over"){
-							window.location = "result.php";
-						}	
-						else {
-							document.getElementById("load_questions").innerHTML = xmlhttp.responseText;
-							load_total_question();
-						}
-					}
-				};
-				xmlhttp.open("GET","../../ajax/load_questions.php?questionNo=" + questionNo, true);
-				xmlhttp.send(null); 
-			 }
-
-			//function to collect checked answer
-			 function radioclick(radiovalue, questionNo) 
-			 {
-				var xmlhttp=new XMLHttpRequest();
-				xmlhttp.onreadystatechange=function() {
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						
-					}
-				};
-				xmlhttp.open("GET","../../ajax/save_answer.php?questionNo=" + questionNo + "&value1="+radiovalue, true);
-				xmlhttp.send(null);
-
-			 }
-
-			 
-			 // funtion to display next and previous questions
-			 function load_previous()
-			 {
-				if(questionNo == "1") 
-				{
-					load_questions(questionNo);
-				}
-				else {
-					questionNo=eval(questionNo)-1;
-					load_questions(questionNo);
-				}
-			 }
-
-			 function load_next()
-			 {
-				questionNo=eval(questionNo)+1;
-				load_questions(questionNo);
-			 }
-	</script>
-
-	
+	<div class="footer-section">
+		<?php include '../../includes/footer.php'; ?>
+	</div>
+	<!-- JS script for bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
--- wbee/users/teacher/teachers.php
+++ wbee/users/teacher/teachers.php
@@ -1,7 +1,5 @@
 <?php
-	require '../../resources/config.php';
-	if(empty($_SESSION['email']))
-		header('Location: ../../login.php');
+	include '../../resources/config.php';
 ?>
 
 <html>
@@ -11,45 +9,63 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="sstyle.css">
 </head>
-	
-<body>
-	<div class="header-section">
-		<?php include 'theader.php'; ?>
-	</div>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-2">
-				<div class="wrapper">
-    				<div class="sidebar">
-        				<?php include 'tsidebar.php'; ?>
-    				</div>
-				</div>
-			</div>
-			<div class="col-sm-10">
-				<div class="main-container">
-					<h2>This is Dashboard for teachers</h2>
-					<div class="card text-center">
-					<div class="card-header">
-						Dashboard
-					</div>
-					<div class="card-body">
-						<h2>content here.</h2>
-					</div>
-					<div class="card-footer text-muted">
-						2 days ago
-					</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>    
+<div>
+	<?php include "theader.php"; ?>
+</div>
 
-	<div class="footer-section">
-		<?php include '../../includes/footer.php'; ?>
-	</div>
-	<!-- JS script for bootstrap -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <?php include 'tsidebar.php'; ?>
+        </div>
+        <div class="col-sm-9">
+            <div class="container">
+			<center>
+			<h1>Students Performance</h1>
+
+			</center>
+			
+				<?php 
+				 $count = 0;
+				 $sql = "SELECT * FROM exam_result ORDER BY id DESC";
+				 $stmt = $connect->query($sql);
+				 $count = $stmt->rowCount();
+				 if($count == 0) 
+				 {
+					 ?> 
+					 <center>
+						<h1>No results available</h1>
+					</center>
+					 <?php
+				 }
+				 else {
+					 echo "<table class='table table-borderdered'>";
+					 echo "<tr>";
+					 echo "<th>"; echo "Exam"; echo "<th>";
+					 echo "<th>"; echo "Total Questions"; echo "<th>";
+					 echo "<th>"; echo "Correct Answers"; echo "<th>";
+					 echo "<th>"; echo "Wrong Answers"; echo "<th>";
+					 echo "<th>"; echo "Date"; echo "<th>";
+					 echo "<tr>";
+					 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+						echo "<tr>";
+					 	echo "<td>"; echo $row["exam_type"]; echo "<td>";
+					 	echo "<td>"; echo $row["total_question"]; echo "<td>";
+					 	echo "<td>"; echo $row["correct_answer"]; echo "<td>";
+					 	echo "<td>"; echo $row["wrong_answer"]; echo "<td>";
+					 	echo "<td>"; echo $row["exam_time"]; echo "<td>";
+					 	echo "<tr>";
+					 }
+					 echo "</table>";
+				 }
+				?>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+	<?php include '../../includes/footer.php'; ?>
+</div>
 </html>
